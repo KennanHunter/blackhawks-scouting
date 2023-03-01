fa4a9c97a6205d18a82c6830c3a37cfcd22f4238
Kennan Hunter <kennanhunter5@gmail.com>
2023-02-28 19:23:54 -0500

    changes

--- client/src/views/UploadView.vue
+++ client/src/views/UploadView.vue
@@ -64,7 +64,7 @@ export default {
                             body,
                         }
                     ).then((val) => {
-                        console.dir(val);
+                        console.dir({ val });
                     });
                 } catch (error) {
                     console.error(error);
--- server/src/csv.ts
+++ server/src/csv.ts
@@ -1,16 +1,33 @@
+import { CSV } from "@scout/shared";
+
 export const collateCSVs = (...csvs: string[]): string => {
     const header = csvs[0].split("\n")[0];
+
+    const scoutedTimeIndex =
+        CSV.deserialize(header).header.indexOf("ScoutedTime");
 
     const recordsWithoutHeaders = csvs
         .map((val) => val.split("\n").slice(1))
         .flat();
 
-    const sortedRecordsWithoutHeaders = recordsWithoutHeaders.sort((a, b) => {
-        const aTimestamp = Number.parseInt(a.split('"').slice(-2, -1)[0]);
-        const bTimestamp = Number.parseInt(b.split('"').slice(-2, -1)[0]);
+    const sortedRecordsWithoutHeaders = recordsWithoutHeaders
+        .sort((a, b) => {
+            const aTimestamp = Number.parseInt(
+                CSV.deserialize(a).header[scoutedTimeIndex]
+            );
+            const bTimestamp = Number.parseInt(
+                CSV.deserialize(b).header[scoutedTimeIndex]
+            );
 
-        return aTimestamp - bTimestamp;
-    });
+            return aTimestamp - bTimestamp;
+        })
+        .reduce((prev, cur) => {
+            if (prev.includes(cur)) {
+                return prev;
+            }
+
+            return [...prev, cur];
+        }, [] as string[]);
 
     return [header, ...sortedRecordsWithoutHeaders].join("\n");
 };
